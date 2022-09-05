cef22e262437108f43229fda499eba44c425b7c1
Michał Nierebiński <contact@caninto.space>
2022-09-05 18:24:56 +0200

    Initial Commit

---
+++ Chirped/ChirpedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChirpedApp.swift
+//  Chirped
+//
+//  Created by Michal Nierebinski on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChirpedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
