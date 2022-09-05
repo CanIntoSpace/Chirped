613c4a2795e1f0c8fb2113c4cda411e820fad9fa
Michał Nierebiński <contact@caninto.space>
2022-09-05 20:05:50 +0200

    Keys

---
+++ Chirped/Settings/Settings.swift
@@ -0,0 +1,21 @@
+//
+//  Settings.swift
+//  Chirped
+//
+//  Created by Michal Nierebinski on 05/09/2022.
+//
+
+import Foundation
+
+enum Settings {
+    static let bearerToken = "XXXXX"
+    enum API {
+        static let key = "XXXXX"
+        static let secret = "XXXXX"
+    }
+
+    enum Client {
+        static let id = "XXXXX"
+        static let secret = "XXXXX"
+    }
+}
